15243730092b7f384d3da3f3fbe32200a2f5df6e
Tionlab <tionlab02@gmail.com>
2023-08-25 05:28:34 +0900

    fixed

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const router = useRouter();
 
-  const startQuiz = () => {
+  useEffect(() => {
     router.push(`/quiz`);
-  };
+  }, []); 
 
   return (
     <div className={styles.container}>
-      <h1>Face Quiz</h1>
-      <button onClick={startQuiz}>Start Quiz</button>
+      <h1>loading...</h1>
     </div>
   );
 }
